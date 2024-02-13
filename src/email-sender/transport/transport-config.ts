04d442ff66ee7a69f0251bd2e00ad1127e510cf6
Pedro Luis De Leon Alejo <Breidydl@gmail.com>
2024-02-12 23:16:35 -0400

    updated

--- src/email-sender/transport/transport-config.ts
+++ src/email-sender/transport/transport-config.ts
@@ -1,9 +1,11 @@
+import 'dotenv/config';
+
 export const transportConfig = {
   host: 'smtp.gmail.com',
   port: 465,
   secure: true,
   auth: {
-    user: 'pedro.sidesys@gmail.com',
-    pass: 'tnymjbarsaldcrxc',
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASSWORD,
   },
 };
